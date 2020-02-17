abe10c178a390d588e0d905fd4acb5612ebd2f3c
Ondřej Bazala <bazala@inqool.cz>
2020-02-17 17:28:09 +0100

    init

---
+++ src/main/java/cz/bazala/ondra/App.java
@@ -0,0 +1,13 @@
+package cz.bazala.ondra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cz/bazala/ondra/AppTest.java
@@ -0,0 +1,20 @@
+package cz.bazala.ondra;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
